ec25e21d13efa7d3fd12658490a1ef06de754a00
snipher <sniphermarube@gmail.com>
2024-06-02 14:08:06 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+export const content = [
+  "./templates/**/*.html",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
