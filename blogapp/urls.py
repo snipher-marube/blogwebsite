c9e359cb34ab13ffa4aac8a9699a3c968381e4bc
snipher <sniphermarube@gmail.com>
2024-06-07 08:55:33 +0300

    html template designs

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('detail/', views.post_detail, name='detail'),
+    path('category/', views.post_by_category, name='category'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'blogapp/home.html', context)
+
+def post_by_category(request):
+    context = {}
+    return render(request, 'blogapp/post_by_category.html', context)
+
+def post_detail(request):
+    context = {}
+    return render(request, 'blogapp/post_detail.html', context)
