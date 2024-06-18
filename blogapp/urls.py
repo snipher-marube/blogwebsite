add58bafc89c20182d7d28b34c69c91efe86ff6d
snipher <sniphermarube@gmail.com>
2024-06-18 11:29:01 +0300

    post categorization

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<slug:category_slug>/<slug:slug>/<int:year>/<int:month>/<int:day>/', views.post_detail, name='detail'),
-    path('category/', views.post_by_category, name='category'),
+    path('category/<slug:slug>/', views.posts_by_category, name='posts_by_category'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 def home(request):
@@ -11,12 +11,24 @@ def home(request):
 
 def post_detail(request, category_slug, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, publish__year=year, publish__month=month, publish__day=day, category__slug=category_slug, status='published')
+    # get all posts in that category as same as this post
+    # exclude the current post
+    post_category = post.category
+    related_posts = Post.published.filter(category=post_category).exclude(id=post.id)
+    
     context = {
-        'post': post
+        'post': post,
+        'related_posts': related_posts
     }
     return render(request, 'blogapp/post_detail.html', context)
 
-def post_by_category(request):
-    context = {}
+def posts_by_category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    posts = Post.published.filter(category=category)
+        
+    context = {
+        'category': category,
+        'posts': posts
+    }
     return render(request, 'blogapp/post_by_category.html', context)
 
